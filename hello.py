d39abcdc61016fe715a6ae77d6cd1e0652745f2a
Danzels112 <69958076+Danzels112@users.noreply.github.com>
2023-02-07 15:13:18 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
-def toyou(x):
+def toyou(x_val):
     """ Function providing greeting """
-    return "hi %s" % x
+    return f"hi {x_val}"
 
 
-def add(x):
+def add(x_val):
     """ Function for addition """    
-    return x + 1
+    return x_val + 1
 
 
-def subtract(x):
+def subtract(x_val):
     """ Function for substraction """    
-    return x - 1
+    return x_val - 1
